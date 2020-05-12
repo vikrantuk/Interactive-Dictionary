0a4e15aa6ef318e95c69f46c4299747f8fabe311
Vikrant Karawande <viku1996@gmail.com>
2020-05-12 11:31:29 +0530

    Handling words not in dictionary

--- app1.py
+++ app1.py
@@ -3,7 +3,10 @@ import json
 data = json.load(open("data.json"))
 
 def translate(word):
-    return data[word]
+    if word in data:
+        return data[word]
+    else:
+        return "Word doesn't exist. Please check for speliing errors.."
 
 word = input("Enter word: ")
 
