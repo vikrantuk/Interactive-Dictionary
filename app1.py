26d1f86f0de94422e887e3381f4bc2e0a668d337
Vikrant Karawande <viku1996@gmail.com>
2020-05-14 09:32:50 +0530

    Added ability to handle nouns and acronyms

--- app1.py
+++ app1.py
@@ -6,6 +6,10 @@ data = json.load(open("data.json"))
 def translate(word):
     if word in data:
         return data[word]
+    elif word.capitalize() in data:
+        return data[word.capitalize()]
+    elif word.upper() in data:
+        return data[word.upper()]
     else:
         li = get_close_matches(word,data.keys(),n=1,cutoff=0.8)
         if(len(li)>0 and input(f"Did you mean {li[0]}? y/n: ").lower()=='y'):
